d8798ce16f4fdeaf7a9f4453b997cb3376f6dd24
AceBee007 <yk.zhang@media.is.uec.ac.jp>
2020-01-15 18:42:07 +0900

    update ver No.

--- easy2use_func/__init__.py
+++ easy2use_func/__init__.py
@@ -1,7 +1,7 @@
 from easy2use_func import *
 
 __copyright__    = 'Copyright (C) 2018 yk.zhang'
-__version__      = '0.0.1'
+__version__      = '0.0.2'
 __license__      = 'GNU GPL v3'
 __author__       = 'yk.zhang'
 __author_email__ = 'yk.zhang@uec.ac.jp'
